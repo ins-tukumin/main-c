f9c611a9b10b19d63b3794c21cbfd3243f500d23
ins-tukumin <ins.tukumin@gmail.com>
2024-09-12 18:17:14 +0900

    update

--- main-rep.py
+++ main-rep.py
@@ -67,13 +67,13 @@ template = """
     {chat_history}
     </hs>
     ------
-    {message}
+    {question}
     Answer:
     """
 
 # 会話のテンプレートを作成
 prompt = PromptTemplate(
-    input_variables=["chat_history", "context", "message"],
+    input_variables=["chat_history", "context", "question"],
     template=template,
 )
 select_model = "gpt-4o"
