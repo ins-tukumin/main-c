6a3895ed7e7c581028f35d771361ee1ffb4d7ae7
ins-tukumin <ins.tukumin@gmail.com>
2024-09-18 13:09:52 +0900

    update

---
+++ main-rep-2.py
@@ -0,0 +1,237 @@
+import pysqlite3
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import os
+import streamlit as st
+from streamlit_chat import message
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import datetime
+import pytz
+import time
+
+#現在時刻
+global now
+now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+
+if "generated" not in st.session_state:
+    st.session_state.generated = []
+    st.session_state.initge = []
+if "past" not in st.session_state:
+    st.session_state.past = []
+
+# クエリパラメータからユーザーIDを取得
+#query_params = st.query_params
+query_params = st.experimental_get_query_params()
+user_id = query_params.get('user_id', [None])[0]
+group = query_params.get('group', [None])[0]
+is_second = 'second' in query_params
+user_id = int(user_id)
+
+if "initialized" not in st.session_state:
+    st.session_state['initialized'] = False
+    initial_message = "今日の振り返りをしよう！今日はどんな一日だった？"
+    st.session_state.initge.append(initial_message)
+    st.session_state['initialized'] = True
+
+
+
+
+# テンプレートの設定
+template = """
+    今日の出来事を振り返って、ユーザーに自由に感想を語ってもらいましょう。適度な問いかけを行って、会話を促進してください。
+    100字以内で話してください。
+    私が書いた日記を適宜抜粋して提供します。この情報も手助けにして、私のことを理解しようとしてみてください。
+    さらに、この会話では「エピソード記憶」を積極的に話題に出して会話してほしいです。これは、日記の内容を基にエピソード記憶を含む内容を会話に登場させることで親密さを高めたりすることを目的としています。
+    「エピソード記憶」やそれに類似した単語を直接的に文脈に含めないでください。
+    今回のテーマは、今日の振り返りなので、エピソード記憶の詳細を深掘るような問いかけではなく、今日の振り返りにつながるように話題に出してほしいです。
+    エピソード記憶という言葉の意味は以下に示します。
+    # エピソード記憶とは、人間の記憶の中でも特に個人的な経験や出来事を覚える記憶の種類の一つです。エピソード記憶は、特定の時間と場所に関連する出来事を含む記憶であり、過去の個人的な経験を詳細に思い出すことができる記憶を指します。
+    同じエピソード記憶に関する参照を何度も会話に出すことは控えてください。適切な頻度を見極めて言及をお願いします。
+    毎回同じ内容を話すと私にとっては不自然な会話となってしまいます。
+    あなたが私の日記を読んでいることを直接的に言わないでください。それとなく日記の内容を基に理解を深め、発言内容に反映させてください。
+    敬語は使わないでください。私の友達になったつもりで砕けた口調で話してください。
+    １００文字程度で話してください。
+    日本語で話してください。
+    私の入力に基づき、次の文脈（<ctx></ctx>で囲まれた部分）とチャット履歴（<hs></hs>で囲まれた部分）を使用して回答してください。:
+    ------
+    <ctx>
+    {context}
+    </ctx>
+    ------
+    <hs>
+    {chat_history}
+    </hs>
+    ------
+    {question}
+    Answer:
+    """
+
+# 会話のテンプレートを作成
+prompt = PromptTemplate(
+    input_variables=["chat_history", "context", "question"],
+    template=template,
+)
+select_model = "gpt-4o"
+select_temperature = 0.5
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+if user_id:
+    if not firebase_admin._apps:
+        # 環境変数を読み込む
+        type = st.secrets["type"]
+        project_id = st.secrets["project_id"]
+        private_key_id = st.secrets["private_key_id"]
+        private_key = st.secrets["private_key"].replace('\\n', '\n')
+        client_email = st.secrets["client_email"]
+        client_id = st.secrets["client_id"]
+        auth_uri = st.secrets["auth_uri"]
+        token_uri = st.secrets["token_uri"]
+        auth_provider_x509_cert_url = st.secrets["auth_provider_x509_cert_url"]
+        client_x509_cert_url = st.secrets["client_x509_cert_url"]
+        universe_domain = st.secrets["universe_domain"]
+        # Firebase認証情報を設定
+        cred = credentials.Certificate({
+            "type": type,
+            "project_id": project_id,
+            "private_key_id": private_key_id,
+            "private_key": private_key,
+            "client_email": client_email,
+            "client_id": client_id,
+            "auth_uri": auth_uri,
+            "token_uri": token_uri,
+            "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+            "client_x509_cert_url": client_x509_cert_url,
+            "universe_domain": universe_domain
+            })
+        default_app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    
+    db_path = f"./vector_lab_databases/{user_id}"
+    if os.path.exists(db_path):
+        embeddings = OpenAIEmbeddings(
+            model="text-embedding-3-large",
+        )
+
+        database = Chroma(
+            persist_directory=db_path,
+            embedding_function=embeddings,  # エンベディング関数を提供
+        )
+
+        chat = ChatOpenAI(
+            model=select_model,
+            temperature=select_temperature,
+        )
+
+        retriever = database.as_retriever()
+
+        if "memory" not in st.session_state:
+            st.session_state.memory = ConversationBufferMemory(
+                memory_key="chat_history",
+                return_messages=True,
+            )
+
+        memory = st.session_state.memory
+
+        chain = ConversationalRetrievalChain.from_llm(
+            llm=chat,
+            retriever=retriever,
+            memory=memory,
+            combine_docs_chain_kwargs={'prompt': prompt}
+        )
+
+        #--------------------------------------------
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+        if "generated" not in st.session_state:
+            st.session_state.generated = []
+        if "past" not in st.session_state:
+            st.session_state.past = []
+        if "count" not in st.session_state:
+            st.session_state.count = 0
+
+        def on_input_change():
+            st.session_state.count += 1
+            user_message = st.session_state.user_message
+            with st.spinner("相手からの返信を待っています。。。"):
+                response = chain({"question": user_message})
+                response_text = response["answer"]
+            st.session_state.past.append(user_message)
+            st.session_state.generated.append(response_text)
+            st.session_state.user_message = ""
+            #st.session_state["user_message"] = ""
+            Agent_1_Human_Agent = "Human" 
+            Agent_2_AI_Agent = "AI" 
+            doc_ref = db.collection(str(user_id)).document(str(now))
+            doc_ref.set({
+                Agent_1_Human_Agent: user_message,
+                Agent_2_AI_Agent: response_text
+            })
+        # 会話履歴を表示するためのスペースを確保
+        chat_placeholder = st.empty()
+        # 会話履歴を表示
+        with chat_placeholder.container():
+            message(st.session_state.initge[0], key="init_greeting_plus", avatar_style="micah")
+            for i in range(len(st.session_state.generated)):
+                message(st.session_state.past[i], is_user=True, key=str(i), avatar_style="adventurer", seed="Nala")
+                key_generated = str(i) + "keyg"
+                message(st.session_state.generated[i], key=str(key_generated), avatar_style="micah")
+                
+        with st.container():
+            if st.session_state.count >= 5:
+                group_url = "https://nagoyapsychology.qualtrics.com/jfe/form/SV_5cZeI9RbaCdozTU"
+                group_url_with_id = f"{group_url}?user_id={user_id}&group={group}"
+                st.markdown(f'これで今回の会話は終了です。こちらをクリックしてアンケートに回答してください。: <a href="{group_url_with_id}" target="_blank">リンク</a>', unsafe_allow_html=True)
+            else:
+                user_message = st.text_area("内容を入力して送信ボタンを押してください", key="user_message")
+                st.button("送信", on_click=on_input_change)
+    else:
+        st.error(f"No vector database found for student ID {user_id}.")
+
+
