b2c617f51899a5cbdd8aa5c028360b220a9a08d7
ins-tukumin <ins.tukumin@gmail.com>
2024-11-22 13:13:03 +0900

    update

--- login_diary.py
+++ login_diary.py
@@ -3,11 +3,12 @@ import streamlit as st
 import random
 
 # 特定のURLを定義します
-special_url = "https://nagoyapsychology.qualtrics.com/jfe/form/SV_55DnU55WeDglj4G"
+# special_url = "https://nagoyapsychology.qualtrics.com/jfe/form/SV_55DnU55WeDglj4G"
+special_url = "https://chat-c.streamlit.app/"
 
 # Streamlitアプリケーションのレイアウト
 st.title("ログインページ")
-st.subheader("前半フェーズ４日目")
+st.subheader("1日目")
 
 # ユーザー入力を受け取る
 user_id = st.text_input("クラウドワークスIDを入力してください")
